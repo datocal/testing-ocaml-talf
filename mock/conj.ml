4c42f3a4629ce47e31b00868f491c18c0588adc0
David Touriño <david.tourino@udc.es>
2017-12-20 16:18:36 +0100

    Intento de mock fallido

--- mock/conj.ml
+++ mock/conj.ml
@@ -4,16 +4,83 @@ type 'a conjunto = Conjunto of 'a list;;
 
 let conjunto_vacio = (Conjunto []);;
 
+type simbolo =
+     Terminal of string 
+   | No_terminal of string;;
+
+(*****************************************************************************)
+
+type er =
+     Vacio
+   | Constante of simbolo
+   | Union of (er * er)
+   | Concatenacion of (er * er)
+   | Repeticion of er;;
+
+(*****************************************************************************)
+
+type estado =
+   Estado of string;;
+
+type arco_af =
+   Arco_af of (estado * estado * simbolo);;
+
+type af =
+   Af of (estado conjunto *
+          simbolo conjunto * 
+          estado *
+          arco_af conjunto * 
+          estado conjunto);;
+
+(*****************************************************************************)
+
+type regla_gic =
+   Regla_gic of (simbolo * simbolo list);;
+
+type gic =
+   Gic of (simbolo conjunto * 
+           simbolo conjunto *
+           regla_gic conjunto * 
+           simbolo);;
+
+(*****************************************************************************)
+
+type arco_ap =
+   Arco_ap of (estado * estado * simbolo * simbolo * simbolo list);;
+
+type ap = 
+   Ap of (estado conjunto * simbolo conjunto * simbolo conjunto *
+          estado * arco_ap conjunto * simbolo * estado conjunto);;
+
+(*****************************************************************************)
+
+type movimiento_mt =
+     Izquierda
+   | Derecha;;
+
+type arco_mt =
+   Arco_mt of (estado * estado * simbolo * simbolo * movimiento_mt);;
+
+type mt =
+   Mt of (estado conjunto * simbolo conjunto * simbolo conjunto *
+          estado * arco_mt conjunto * simbolo * estado conjunto);;
+
+
+
 let es_vacio= function
 	(Conjunto []) -> true
- 	|_-> raise (Not_Mocked "Es vac�o");;
+ 	|_-> raise (Not_Mocked "Es vacío");;
 
-let pertenece s ns = match s,ns with 
+let pertenece s, ns=  match s,ns with 
+   (Estado "0"), (Conjunto [Estado "0"]) -> true
+  | (Estado "1"), (Conjunto [Estado "0"]) -> false    
+  
 	| _ -> raise (Not_Mocked "Pertenece ");;
 
 
 let agregar a conj = match a,conj with
-   _ -> raise (Not_Mocked "Agregar");;
+   (Estado "1"), conjunto_vacio -> (Conjunto [Estado "1"])
+   |_ -> raise (Not_Mocked "Agregar");;
 (*****************************************************************************)
 
 let rec conjunto_of_list = function
@@ -37,7 +104,7 @@ let union a b = match a,b with
 (*****************************************************************************)
 
 let rec interseccion a b = match a,b with
-    _-> raise (Not_Mocked "Intersección");;
+    _-> raise (Not_Mocked "IntersecciÃ³n");;
 
 (*****************************************************************************)
 
